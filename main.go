ff4e1165e975daa8d7a364df443bd3abc57ff832
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-07-22 11:37:35 -0700

    feat: add check to `snippetView` function

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // home() is a regular Go function with two parameters
@@ -17,6 +19,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	// Extract the `id` wildcard from user request using r.PathValue()
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	msg := fmt.Sprintf("Display specific snippet with ID %d", id)
+	w.Write([]byte(msg))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
 func main() {
 
 	// A ServeMux is an HTTP request multiplexer.
@@ -24,7 +42,11 @@ func main() {
 	// When an incoming user request matches a registered pattern, it
 	// calls and executes the mapped handler function.
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+
+	// View the snippet with a particular ID
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Println("Starting server on :4000")
 
