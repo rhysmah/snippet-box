56bb2f817d475db015a13fd15d2a8ef31a7a18ed
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-07-23 10:23:29 -0700

    feat: add HTML template, add parsing to home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"http/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from SnippetBox"))
+
+	// The template.ParseFiles() function reads the template and creates
+	// a template.Template object, which has template-specific functions that
+	// allow us to interact and executen the template. If there's an error
+	// parsing the template, we log the error, then send the user an error
+	// message with the "Internal Server Error" error code.
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute() is used to write the template to the response body,
+	// which the reader receives and renders the template to their browser.
+	// The last parameter to Execute represents dynamic data to be rendered
+	// in the template
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
