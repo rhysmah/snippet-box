e4c247d7043fbce57f43f3c01ede6ef942c2406b
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-07-23 11:46:21 -0700

    feat: create fileServer to access static web elements

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	// Creates a file server that serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	//
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", home) // Requires exact match
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
