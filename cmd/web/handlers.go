8db46c66bf168a43a5b95a38f71bfcbcfc3c12fc
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-07-23 11:15:50 -0700

    feat: add various

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,12 +11,18 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
+	webFiles := []string{
+		"./ui/html/base.tmpl.html", // base template must be first
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
 	// The template.ParseFiles() function reads the template and creates
 	// a template.Template object, which has template-specific functions that
-	// allow us to interact and executen the template. If there's an error
+	// allow us to interact and execute the template. If there's an error
 	// parsing the template, we log the error, then send the user an error
 	// message with the "Internal Server Error" error code.
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl.html")
+	ts, err := template.ParseFiles(webFiles...)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -27,7 +33,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// which the reader receives and renders the template to their browser.
 	// The last parameter to Execute represents dynamic data to be rendered
 	// in the template
-	err = ts.Execute(w, nil)
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
