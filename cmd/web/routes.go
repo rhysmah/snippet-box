2f529305df09c25192c298c49ad6405c07e3808c
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-08-09 09:49:31 -0700

    feat: add initial helpers, handlers for user login

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -20,6 +20,12 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standard.Then(mux)
 }
