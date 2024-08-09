36d98aa69acbee18cabc8c84e9830ebf188c0e6f
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-08-09 09:58:51 -0700

    feat: add user login errors

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,8 @@ import "errors"
 
 // Created so handlers aren't concerned with the underlying
 // datastore or reliant on datastore-specific errors.
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord           = errors.New("models: no matching record found")
+	ErrInvalidCredentials = errors.New("models: invalud credentials")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
+)
