73a5bb2e363e0463ced34a5677ce2e9674698da6
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-08-12 11:10:34 -0700

    feat: add logout functionality

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -220,5 +220,15 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user")
+
+	// Renew session ID
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "You've been logged out successfully!")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
