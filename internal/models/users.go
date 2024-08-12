35bf6ff1ee8032fc0256e6f1abc904f1239a6e6b
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-08-12 11:05:49 -0700

    feat: add user login in functionality

--- internal/models/users.go
+++ internal/models/users.go
@@ -53,7 +53,34 @@ func (m *UserModel) Insert(name, email, password string) error {
 
 // Authenticate a user based on email and password; if the user exists, return ID
 func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	// Return ID and hashed password associated with given email
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, nil
+		}
+	}
+
+	// Check if hashed password and plain-text password match
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	// Else, password is correct; return user ID
+	return id, nil
+
 }
 
 // Checks if user with specific ID exists
